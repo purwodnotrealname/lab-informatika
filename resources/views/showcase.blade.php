3bdf1034de4d95b2b757e1565b63ff548cff9126
Caecill <albertindjema9@gmail.com>
2025-07-11 01:02:33 +0800

    navbar showcase

--- resources/views/showcase.blade.php
+++ resources/views/showcase.blade.php
@@ -26,7 +26,7 @@
                         <a class="nav-link active" href="/showcase">Showcase</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Account</a>
+                        <a class="nav-link" href="/account">Account</a>
                     </li>
                 </ul>
             </div>
